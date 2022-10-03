eba689f96dc204df8c8135ff01520a14237c6306
bigsmile0128 <goldwebnike@gmail.com>
2022-10-03 00:29:03 -0700

    add: add store management-v2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
-import {   Routes , Route } from 'react-router-dom';
-import routes from './pages/index'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import routes from "./pages/index";
+const store = configureStore();
 
 function App() {
-
-    return (
-        <Routes >
-            {
-            routes.map((data,index) => (
-                <Route onUpdate={() => window.scrollTo(0, 0)} exact={true} path={data.path} element={data.component} key={index} />
-            ))
-            }
+  return (
+    <Provider store={store}>
+      <Routes>
+        {routes.map((data, index) => (
+          <Route
+            onUpdate={() => window.scrollTo(0, 0)}
+            exact={true}
+            path={data.path}
+            element={data.component}
+            key={index}
+          />
+        ))}
       </Routes>
-    );
+    </Provider>
+  );
 }
 
 export default App;
